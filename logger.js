809353457ca793dea3a4a84ab3802f0a8b5c54e7
Uzzal Roy <uzzalroy.acm@gmail.com>
2023-04-09 13:02:38 +0600

    Error Logger

---
+++ logger.js
@@ -0,0 +1,49 @@
+import expressWinston from 'express-winston';
+import winston from 'winston';
+import { ElasticsearchTransport } from 'winston-elasticsearch';
+
+const getMessage = (req) => {
+    const obj = {
+        correlationId: req.headers['x-correlation-id'],
+        requestBody: req.body,
+    };
+
+    return JSON.stringify(obj);
+};
+
+const mongoErrorTransport = (uri) => {
+    new winston.transports.MongoDB({
+        db: uri,
+        metaKey: 'meta',
+    });
+};
+
+const HOST = process.env.ELASTICSEARCH_HOST || 'localhost';
+
+const elasticsearchOptions = {
+    level: 'info',
+    clientOpts: { node: `http://${HOST}:9200` },
+    indexPrefix: 'log-parcelkoi',
+};
+
+const esTransport = new ElasticsearchTransport(elasticsearchOptions);
+
+export const infoLogger = () => {
+    expressWinston.logger({
+        transports: [new winston.transports.Console(), esTransport],
+        format: winston.format.combine(winston.format.colorize(), winston.format.json()),
+        meta: false,
+        msg: getMessage,
+    });
+};
+
+export const errorLogger = (uri) => {
+    expressWinston.errorLogger({
+        transports: [new winston.transports.Console(), mongoErrorTransport(uri), esTransport],
+        format: winston.format.combine(winston.format.colorize(), winston.format.json()),
+        meta: true,
+        msg: '{ "correlationId": "{{req.headers["x-correlation-id"]}}", "error": "{{err.message}}" }',
+    });
+};
+
+export default {};
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import app from './app';
 import config from './config';
 import connect from './db';
+import { infoLogger } from './logger';
 import { handleError, handleRequest } from './middlewares/index';
 
 const PORT = config.APP_PORT || 3000;
 
 app.use(handleRequest);
+
+if (config.APP_ENV !== 'development') {
+    app.use(infoLogger());
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!');

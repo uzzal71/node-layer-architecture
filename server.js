d7d97ec9cf605248f47be779067600916e3ed513
Uzzal Roy <uzzalroy.acm@gmail.com>
2023-04-16 09:37:32 +0600

    Model Migration

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import connect from './db';
 import routeConfiguration from './src/routes';
 import config from './src/config';
 import { handleError, handleRequest } from './src/middlewares/index';
+// import Category from './src/models/data-models/Category';
+// import sequelize from './src/config/sequelize';
 
 const PORT = config.APP_PORT || 3000;
 
@@ -20,12 +22,15 @@ routeConfiguration(app);
 
 app.use(handleError);
 
+// Category.sync();
+
 connect()
     .then(() => {
         app.listen(PORT, () => {
             connect();
             console.log('Database connected');
             console.log(`Server running on port ${PORT}`);
+            // sequelize.sync();
         });
     })
     .catch((err) => {
--- src/models/data-models/Category.js
+++ src/models/data-models/Category.js
@@ -1,15 +1,19 @@
 // Category model
-const mongoose = require('mongoose');
+import { DataTypes } from 'sequelize';
+import sequelize from '../../config/sequelize';
 
-const categorySchema = new mongoose.Schema({
-    parent_id: { type: String, default: 0 },
-    name: { type: String, length: 50, required: true },
-    slug: { type: String, length: 50, required: true },
-    index: { type: Number, default: 0 },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
-});
-
-const Category = mongoose.model('Category', categorySchema);
+const Category = sequelize.define(
+    'Category',
+    {
+        parent_id: { type: DataTypes.SMALLINT, allowNull: true, default: 0 },
+        name: { type: DataTypes.STRING, allowNull: false, length: 50 },
+        slug: { type: DataTypes.STRING, allowNull: false, length: 50 },
+        index: { type: DataTypes.SMALLINT, allowNull: true, default: 0 },
+    },
+    {
+        tableName: 'categories',
+        timestamps: true,
+    },
+);
 
 export default Category;
